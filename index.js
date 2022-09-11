f8281f65c5d6fd6acd85817886443c90f68ec90a
abymani <12557637+abymani@users.noreply.github.com>
2022-09-11 18:47:36 +0500

    small change

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+var shareOption = document.querySelector(".share-container");
+var shareButton = document.querySelector(".share-button");
+
+shareButton.addEventListener("click", function(){
+    // this.classList.toggle("active");
+    shareOption.classList.toggle("active");
+})
 
 
-
-function handleShare(){
+// function handleShare(){
     
    
-    let element=document.getElementById('share-id');
-    element.classList.remove("hide");
-}
+//     let element=document.getElementById('share-id');
+//     element.classList.remove("hide");
+// }
